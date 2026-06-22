a47cdc801aaf9a9a8fb15625358b00c10412f3a8
Tonmoy-Roy <roytonmoy901@gmail.com>
2026-06-22 11:26:02 +0600

    unnecessary loader deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,6 @@
 import './App.css'
 import Cursor from './Components/Cursor'
-import { useEffect, useState } from "react";
-import Loader from './Components/Loader';
-
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000); 
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (loading) {
-    return <Loader />
-  }
 
   return (
     <>
